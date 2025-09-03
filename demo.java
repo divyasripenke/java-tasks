bd5d7faacc5f995d5338851a89ed39f99c6782b7
divyasripenke <113667094+divyasripenke@users.noreply.github.com>
2025-09-03 12:55:02 +0530

    latest-updates

--- demo.java
+++ demo.java
@@ -4,10 +4,6 @@ public class demo {
            String sname = "KRISHNA";
            String course = "PYTHON";
            System.out.println( "LEARN HERE AND LEAD ANYWHERE!!!");
-           System.out.println( "LEARN HERE AND LEAD ANYWHERE!!!");
-           System.out.println( "LEARN HERE AND LEAD ANYWHERE!!!");
-           System.out.println( "LEARN HERE AND LEAD ANYWHERE!!!");
-           System.out.println( "LEARN HERE AND LEAD ANYWHERE!!!");
            System.out.println("My Name is: " + sname);
            System.out.println("My Course is: " + course);
            System.out.println( "My Roll Number is: " + rollno);
