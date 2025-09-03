39631817610fee78b030bdd9201ef39dd31a1897
divyasripenke <113667094+divyasripenke@users.noreply.github.com>
2025-09-03 11:51:47 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,7 +3,7 @@ public class demo
            int rollno = 1596;
            String sname = "KRISHNA":
            String course = "PYTHON";
-           System.out.println( "LEARN HERE AND LEAD ANYWHERE!!!);
+           System.out.println( "LEARN HERE AND LEAD ANYWHERE!!!");
            System.out.println("My Name is: " + sname);
            System.out.println("My Course is: " + course);
            System.out.println( "My Roll Number is: " + rollno);
